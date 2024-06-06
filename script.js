d8fc9ad0c040830938eac7e903127895e03e06c6
kleberbambam <166651217+kleberbambam@users.noreply.github.com>
2024-06-06 16:39:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+//2
+let nome = document.getElementById("nome")
+let Resposta = document.getElementById("Resposta")
+
+function Enviar(){
+    Resposta.innerHTML = "Ola, " + nome.value + " tudo bem?"
+}
+ 
+//3
+let r1 = document.getElementById("f2");
+let r2 = document.getElementById("f1");
+
+
+function f2() {
+    f3.    innerHTML = parseInt(f1.value) / 100 + "m.";
+}
+
+//4
+
+
